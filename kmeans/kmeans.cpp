0a75f02a95316083abc521d42e0757e815440d89
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-08-04 23:29:11 +0200

    Add kmeans source code

---
+++ kmeans/kmeans.cpp
@@ -0,0 +1,378 @@
+/*****************************************************************************/
+/*IMPORTANT:  READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.         */
+/*By downloading, copying, installing or using the software you agree        */
+/*to this license.  If you do not agree to this license, do not download,    */
+/*install, copy or use the software.                                         */
+/*                                                                           */
+/*                                                                           */
+/*Copyright (c) 2005 Northwestern University                                 */
+/*All rights reserved.                                                       */
+
+/*Redistribution of the software in source and binary forms,                 */
+/*with or without modification, is permitted provided that the               */
+/*following conditions are met:                                              */
+/*                                                                           */
+/*1       Redistributions of source code must retain the above copyright     */
+/*        notice, this list of conditions and the following disclaimer.      */
+/*                                                                           */
+/*2       Redistributions in binary form must reproduce the above copyright   */
+/*        notice, this list of conditions and the following disclaimer in the */
+/*        documentation and/or other materials provided with the distribution.*/
+/*                                                                            */
+/*3       Neither the name of Northwestern University nor the names of its    */
+/*        contributors may be used to endorse or promote products derived     */
+/*        from this software without specific prior written permission.       */
+/*                                                                            */
+/*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS    */
+/*IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED      */
+/*TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND         */
+/*FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL          */
+/*NORTHWESTERN UNIVERSITY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,       */
+/*INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES          */
+/*(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR          */
+/*SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)          */
+/*HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,         */
+/*STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    */
+/*ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE             */
+/*POSSIBILITY OF SUCH DAMAGE.                                                 */
+/******************************************************************************/
+/*************************************************************************/
+/**   File:         example.c                                           **/
+/**   Description:  Takes as input a file:                              **/
+/**                 ascii  file: containing 1 data point per line       **/
+/**                 binary file: first int is the number of objects     **/
+/**                              2nd int is the no. of features of each **/
+/**                              object                                 **/
+/**                 This example performs a fuzzy c-means clustering    **/
+/**                 on the data. Fuzzy clustering is performed using    **/
+/**                 min to max clusters and the clustering that gets    **/
+/**                 the best score according to a compactness and       **/
+/**                 separation criterion are returned.                  **/
+/**   Author:  Wei-keng Liao                                            **/
+/**            ECE Department Northwestern University                   **/
+/**            email: wkliao@ece.northwestern.edu                       **/
+/**                                                                     **/
+/**   Edited by: Jay Pisharath                                          **/
+/**              Northwestern University.                               **/
+/**                                                                     **/
+/**   ================================================================  **/
+/**																		**/
+/**   Edited by: Sang-Ha  Lee											**/
+/**				 University of Virginia									**/
+/**																		**/
+/**   Description:	No longer supports fuzzy c-means clustering;	 	**/
+/**					only regular k-means clustering.					**/
+/**					Simplified for main functionality: regular k-means	**/
+/**					clustering.											**/
+/**                                                                     **/
+/*************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "getopt.h"
+
+#include "kmeans.h"
+
+extern double wtime(void);
+extern ssize_t read(int, void *, size_t);
+extern int close(int);
+
+/*---< usage() >------------------------------------------------------------*/
+void usage(char *argv0)
+{
+    const char *help =
+        "Usage: \%s [switches] -i filename\n"
+        "       -i filename     :  file containing data to be clustered\n"
+        "       -b                 :input file is in binary format\n"
+        "       -k                 : number of clusters (default is 8) \n"
+        "       -t threshold    : threshold value\n";
+    fprintf(stderr, help, argv0);
+    exit(-1);
+}
+
+/*---< main() >-------------------------------------------------------------*/
+int main(int argc, char **argv)
+{
+    int opt;
+    extern char *optarg;
+    extern int optind;
+    int nclusters = 5;
+    char *filename = 0;
+    float *buf;
+    float **attributes;
+    float **cluster_centres = NULL;
+
+    int numAttributes;
+    int numObjects;
+    char line[1024];
+    int isBinaryFile = 0;
+    int nloops;
+    float threshold = 0.001;
+    double timing;
+
+    while ((opt = getopt(argc, argv, "i:k:t:b")) != EOF)
+    {
+        switch (opt)
+        {
+        case 'i':
+            filename = optarg;
+            break;
+        case 'b':
+            isBinaryFile = 1;
+            break;
+        case 't':
+            threshold = atof(optarg);
+            break;
+        case 'k':
+            nclusters = atoi(optarg);
+            break;
+        case '?':
+            usage(argv[0]);
+            break;
+        default:
+            usage(argv[0]);
+            break;
+        }
+    }
+
+    if (filename == 0)
+        usage(argv[0]);
+
+    numAttributes = numObjects = 0;
+
+    /* from the input file, get the numAttributes and numObjects ------------*/
+
+    if (isBinaryFile)
+    {
+        FILE* infile;
+        if ((infile = fopen(filename, "r")) == NULL)
+        {
+            fprintf(stderr, "Error: no such file (%s)\n", filename);
+            exit(1);
+        }
+        fread(&numObjects, sizeof(int), 1, infile);
+        fread(&numAttributes, sizeof(int), 1, infile);
+
+        /* allocate space for attributes[] and read attributes of all objects */
+        buf = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        attributes = (float **)malloc(numObjects * sizeof(float *));
+        attributes[0] = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        for (int i = 1; i < numObjects; i++)
+            attributes[i] = attributes[i - 1] + numAttributes;
+
+        fread(buf, sizeof(float), numObjects * numAttributes, infile);
+
+        fclose(infile);
+    }
+    else
+    {
+        FILE *infile;
+        if ((infile = fopen(filename, "r")) == NULL)
+        {
+            fprintf(stderr, "Error: no such file (%s)\n", filename);
+            exit(1);
+        }
+        while (fgets(line, 1024, infile) != NULL)
+            if (strtok(line, " \t\n") != 0)
+                numObjects++;
+        rewind(infile);
+        while (fgets(line, 1024, infile) != NULL)
+        {
+            if (strtok(line, " \t\n") != 0)
+            {
+                /* ignore the id (first attribute): numAttributes = 1; */
+                while (strtok(NULL, " ,\t\n") != NULL)
+                    numAttributes++;
+                break;
+            }
+        }
+
+        /* allocate space for attributes[] and read attributes of all objects */
+        buf = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        attributes = (float **)malloc(numObjects * sizeof(float *));
+        attributes[0] = (float *)malloc(numObjects * numAttributes * sizeof(float));
+
+        for (int i = 1; i < numObjects; i++)
+            attributes[i] = attributes[i - 1] + numAttributes;
+
+        rewind(infile);
+
+        int buf_ind = 0;
+
+        while (fgets(line, 1024, infile) != NULL)
+        {
+            if (strtok(line, " \t\n") == NULL)
+                continue;
+            for (int j = 0; j < numAttributes; j++)
+            {
+                buf[buf_ind] = atof(strtok(NULL, " ,\t\n"));
+                buf_ind++;
+            }
+        }
+        fclose(infile);
+    }
+
+    nloops = 1;
+    printf("I/O completed\n");
+
+    memcpy(attributes[0], buf, numObjects * numAttributes * sizeof(float));
+
+    int *membership = (int *)malloc(numObjects * sizeof(int));
+    ;
+
+    float **clusters; /* out: [nclusters][numAttributes] */
+    clusters = (float **)malloc(nclusters * sizeof(float *));
+    clusters[0] = (float *)malloc(nclusters * numAttributes * sizeof(float));
+
+    float **new_centers; /* [nclusters][numAttributes] */
+    new_centers = (float **)malloc(nclusters * sizeof(float *));
+    new_centers[0] = (float *)malloc(nclusters * numAttributes * sizeof(float));
+
+    int *new_centers_len; /* [nclusters]: no. of points in each cluster */
+    new_centers_len = (int *)malloc(nclusters * sizeof(int));
+
+    cluster_centres = NULL;
+
+    //-------- Allocate memory before this line -------------//
+
+    for (int loop = 0; loop < nloops; loop++)
+    {
+
+        // cluster(numObjects,
+        //         numAttributes,
+        //         attributes,           /* [numObjects][numAttributes] */
+        //         nclusters,
+        //         threshold,
+        //         &cluster_centres
+        //        );
+
+        srand(7);
+
+        // tmp_cluster_centres = kmeans_clustering(attributes,
+        //                                         numAttributes,
+        //                                         numObjects,
+        //                                         nclusters,
+        //                                         threshold,
+        //                                         membership);
+        int n = 0, index;
+        float delta;
+
+        /* allocate space for returning variable clusters[] */
+        for (int i = 1; i < nclusters; i++)
+            clusters[i] = clusters[i - 1] + numAttributes;
+
+        /* randomly pick cluster centers */
+        for (int i = 0; i < nclusters; i++)
+        {
+            // n = (int)rand() % numObjects;
+            for (int j = 0; j < numAttributes; j++)
+                clusters[i][j] = attributes[n][j];
+            n++;
+        }
+
+        for (int i = 0; i < numObjects; i++)
+            membership[i] = -1;
+
+        /* need to initialize new_centers_len and new_centers[0] to all 0 */
+        *new_centers_len = 0;
+
+        for (int i = 0; i < nclusters * numAttributes; i++)
+            new_centers[0][i] = 0.0;
+
+        for (int i = 1; i < nclusters; i++)
+            new_centers[i] = new_centers[i - 1] + numAttributes;
+
+        do
+        {
+            delta = 0.0;
+
+            for (int i = 0; i < numObjects; i++)
+            {
+                /* find the index of nestest cluster centers */
+
+                // index = find_nearest_point(attributes[i], numAttributes, clusters, nclusters);
+                float min_dist = FLT_MAX;
+
+                /* find the cluster center id with min distance to pt */
+                for (int k = 0; k < nclusters; k++)
+                {
+                    float dist = 0.0;
+
+                    // dist = euclid_dist_2(attributes[i], clusters[k], numAttributes); /* no need square root */
+                    int i;
+                    for (int coord = 0; coord < numAttributes; coord++)
+                        dist += (attributes[i][coord] - clusters[k][coord]) *
+                                (attributes[i][coord] - clusters[k][coord]);
+                    // euclid_dist_2 end
+
+                    if (dist < min_dist)
+                    {
+                        min_dist = dist;
+                        index = k;
+                    }
+                }
+
+                /* if membership changes, increase delta by 1 */
+                if (membership[i] != index)
+                    delta += 1.0;
+
+                /* assign the membership to object i */
+                membership[i] = index;
+
+                /* update new cluster centers : sum of objects located within */
+                new_centers_len[index]++;
+                for (int j = 0; j < numAttributes; j++)
+                    new_centers[index][j] += attributes[i][j];
+            }
+
+            /* replace old cluster centers with new_centers */
+            for (int i = 0; i < nclusters; i++)
+            {
+                for (int j = 0; j < numAttributes; j++)
+                {
+                    if (new_centers_len[i] > 0)
+                        clusters[i][j] = new_centers[i][j] / new_centers_len[i];
+                    new_centers[i][j] = 0.0; /* set back to 0 */
+                }
+                new_centers_len[i] = 0; /* set back to 0 */
+            }
+
+            // delta /= numObjects;
+        } while (delta > threshold);
+
+        cluster_centres = clusters;
+
+        // kmeans_clustering end
+        // cluster end
+    }
+
+    printf("number of Clusters %d\n", nclusters);
+    printf("number of Attributes %d\n\n", numAttributes);
+    /*printf("Cluster Centers Output\n");
+    printf("The first number is cluster number and the following data is arribute value\n");
+    printf("=============================================================================\n\n");
+
+    for (i=0; i<nclusters; i++) {
+        printf("%d: ", i);
+        for (j=0; j<numAttributes; j++)
+            printf("%f ", cluster_centres[i][j]);
+        printf("\n\n");
+    }*/
+    printf("Time for process: %f\n", timing);
+
+    //------------------ Deallocate memory after this line -------------//
+    free(new_centers[0]);
+    free(new_centers);
+    free(new_centers_len);
+    free(membership);
+    free(attributes);
+    free(cluster_centres[0]);
+    free(cluster_centres);
+    free(buf);
+    return (0);
+}
---
+++ kmeans/kmeans.h
@@ -0,0 +1,219 @@
+/*****************************************************************************/
+/*IMPORTANT:  READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.         */
+/*By downloading, copying, installing or using the software you agree        */
+/*to this license.  If you do not agree to this license, do not download,    */
+/*install, copy or use the software.                                         */
+/*                                                                           */
+/*                                                                           */
+/*Copyright (c) 2005 Northwestern University                                 */
+/*All rights reserved.                                                       */
+
+/*Redistribution of the software in source and binary forms,                 */
+/*with or without modification, is permitted provided that the               */
+/*following conditions are met:                                              */
+/*                                                                           */
+/*1       Redistributions of source code must retain the above copyright     */
+/*        notice, this list of conditions and the following disclaimer.      */
+/*                                                                           */
+/*2       Redistributions in binary form must reproduce the above copyright   */
+/*        notice, this list of conditions and the following disclaimer in the */
+/*        documentation and/or other materials provided with the distribution.*/ 
+/*                                                                            */
+/*3       Neither the name of Northwestern University nor the names of its    */
+/*        contributors may be used to endorse or promote products derived     */
+/*        from this software without specific prior written permission.       */
+/*                                                                            */
+/*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS    */
+/*IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED      */
+/*TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND         */
+/*FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL          */
+/*NORTHWESTERN UNIVERSITY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,       */
+/*INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES          */
+/*(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR          */
+/*SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)          */
+/*HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,         */
+/*STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    */
+/*ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE             */
+/*POSSIBILITY OF SUCH DAMAGE.                                                 */
+/******************************************************************************/
+#ifndef _H_KMEANS
+#define _H_KMEANS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+#include <float.h>
+
+#ifndef FLT_MAX
+#define FLT_MAX 3.40282347e+38
+#endif
+
+#define RANDOM_MAX 2147483647
+
+
+// /* kmeans_clustering.c */
+// __inline
+// float euclid_dist_2(float *pt1,
+//                     float *pt2,
+//                     int    numdims)
+// {
+//     int i;
+//     float ans=0.0;
+
+//     for (i=0; i<numdims; i++)
+//         ans += (pt1[i]-pt2[i]) * (pt1[i]-pt2[i]);
+
+//     return(ans);
+// }
+
+// int find_nearest_point(float  *pt,          /* [nfeatures] */
+//                        int     nfeatures,
+//                        float **pts,         /* [npts][nfeatures] */
+//                        int     npts)
+// {
+//     int index, i;
+//     float min_dist=FLT_MAX;
+
+//     /* find the cluster center id with min distance to pt */
+//     for (i=0; i<npts; i++) {
+//         float dist;
+//         dist = euclid_dist_2(pt, pts[i], nfeatures);  /* no need square root */
+//         if (dist < min_dist) {
+//             min_dist = dist;
+//             index    = i;
+//         }
+//     }
+//     return(index);
+// }
+
+// /*----< euclid_dist_2() >----------------------------------------------------*/
+// /* multi-dimensional spatial Euclid distance square */
+
+
+
+// /*----< kmeans_clustering() >---------------------------------------------*/
+// float** kmeans_clustering(float **feature,    /* in: [npoints][nfeatures] */
+//                           int     nfeatures,
+//                           int     npoints,
+//                           int     nclusters,
+//                           float   threshold,
+//                           int    *membership) /* out: [npoints] */
+// {
+
+//     int      i, j, n=0, index, loop=0;
+//     int     *new_centers_len; /* [nclusters]: no. of points in each cluster */
+//     float    delta;
+//     float  **clusters;   /* out: [nclusters][nfeatures] */
+//     float  **new_centers;     /* [nclusters][nfeatures] */
+  
+
+//     /* allocate space for returning variable clusters[] */
+//     clusters    = (float**) malloc(nclusters *             sizeof(float*));
+//     clusters[0] = (float*)  malloc(nclusters * nfeatures * sizeof(float));
+//     for (i=1; i<nclusters; i++)
+//         clusters[i] = clusters[i-1] + nfeatures;
+
+//     /* randomly pick cluster centers */
+//     for (i=0; i<nclusters; i++) {
+//         //n = (int)rand() % npoints;
+//         for (j=0; j<nfeatures; j++)
+//             clusters[i][j] = feature[n][j];
+// 		n++;
+//     }
+
+//     for (i=0; i<npoints; i++)
+// 		membership[i] = -1;
+
+//     /* need to initialize new_centers_len and new_centers[0] to all 0 */
+//     new_centers_len = (int*) calloc(nclusters, sizeof(int));
+
+//     new_centers    = (float**) malloc(nclusters *            sizeof(float*));
+//     new_centers[0] = (float*)  calloc(nclusters * nfeatures, sizeof(float));
+//     for (i=1; i<nclusters; i++)
+//         new_centers[i] = new_centers[i-1] + nfeatures;
+ 
+  
+//     do {
+		
+//         delta = 0.0;
+
+//         for (i=0; i<npoints; i++) {
+// 	        /* find the index of nestest cluster centers */
+// 	        index = find_nearest_point(feature[i], nfeatures, clusters, nclusters);
+// 	        /* if membership changes, increase delta by 1 */
+// 	        if (membership[i] != index) delta += 1.0;
+
+// 	        /* assign the membership to object i */
+// 	        membership[i] = index;
+
+// 	        /* update new cluster centers : sum of objects located within */
+// 	        new_centers_len[index]++;
+// 	        for (j=0; j<nfeatures; j++)          
+// 				new_centers[index][j] += feature[i][j];
+//         }
+      
+
+// 	/* replace old cluster centers with new_centers */
+//         for (i=0; i<nclusters; i++) {
+//             for (j=0; j<nfeatures; j++) {
+//                 if (new_centers_len[i] > 0)
+// 					clusters[i][j] = new_centers[i][j] / new_centers_len[i];
+// 				new_centers[i][j] = 0.0;   /* set back to 0 */
+// 			}
+// 			new_centers_len[i] = 0;   /* set back to 0 */
+// 		}
+            
+//         //delta /= npoints;
+//     } while (delta > threshold);
+
+  
+//     free(new_centers[0]);
+//     free(new_centers);
+//     free(new_centers_len);
+
+//     return clusters;
+// }
+
+// /* cluster.c */
+// /*---< cluster() >-----------------------------------------------------------*/
+// int cluster(int      numObjects,      /* number of input objects */
+//             int      numAttributes,   /* size of attribute of each object */
+//             float  **attributes,      /* [numObjects][numAttributes] */
+//             int      num_nclusters,
+//             float    threshold,       /* in:   */
+//             float ***cluster_centres /* out: [best_nclusters][numAttributes] */
+    
+//             )
+// {
+//     int     nclusters;
+//     int    *membership;
+//     float **tmp_cluster_centres;
+
+//     membership = (int*) malloc(numObjects * sizeof(int));
+
+//     nclusters=num_nclusters;
+
+//     srand(7);
+	
+//     tmp_cluster_centres = kmeans_clustering(attributes,
+//                                             numAttributes,
+//                                             numObjects,
+//                                             nclusters,
+//                                             threshold,
+//                                             membership);
+
+//     if (*cluster_centres) {
+// 		free((*cluster_centres)[0]);
+//         free(*cluster_centres);
+// 	}
+// 	*cluster_centres = tmp_cluster_centres;
+
+   
+//     free(membership);
+
+//     return 0;
+// }
+
+#endif
