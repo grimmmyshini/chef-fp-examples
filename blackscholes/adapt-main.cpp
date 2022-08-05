0fb38ad898f63d3222441fa387a4c73b4a128fc5
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-08-06 00:18:37 +0200

    Add adapt instrumentation to blackscholes

---
+++ blackscholes/adapt-main.cpp
@@ -0,0 +1,217 @@
+// Copyright (c) 2007 Intel Corp.
+
+// Black-Scholes
+// Analytical method for calculating European Options
+//
+//
+// Reference Source: Options, Futures, and Other Derivatives, 3rd Edition, Prentice
+// Hall, John C. Hull,
+
+#include <cstdio>
+#include <cstring>
+
+#include "blackscholes.hpp"
+
+#include "adapt.h"
+
+// Precision to use for calculations
+
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv)
+{
+    FILE *file;
+    int i;
+    int loopnum;
+    AD_real *buffer;
+    int *buffer2;
+    int rv;
+
+    OptionData *data;
+    AD_real *prices;
+    int numOptions;
+
+    int *otype;
+    AD_real *sptprice;
+    AD_real *strike;
+    AD_real *rate;
+    AD_real *volatility;
+    AD_real *otime;
+    int numError = 0;
+    int nThreads;
+
+    printf("PARSEC Benchmark Suite\n");
+    fflush(NULL);
+
+    if (argc != 2)
+    {
+        printf("Usage:\n\t%s <inputFile>\n", argv[0]);
+        exit(1);
+    }
+    nThreads = 1;
+    char *inputFile = argv[1];
+    const char *outputFile = "prices.output";
+
+    // Read input data from file
+    file = fopen(inputFile, "r");
+    if (file == NULL)
+    {
+        printf("ERROR: Unable to open file `%s'.\n", inputFile);
+        exit(1);
+    }
+    rv = fscanf(file, "%i", &numOptions);
+    if (rv != 1)
+    {
+        printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+        fclose(file);
+        exit(1);
+    }
+    if (nThreads > numOptions)
+    {
+        printf("WARNING: Not enough work, reducing number of threads to match number of options.\n");
+        nThreads = numOptions;
+    }
+
+    if (nThreads != 1)
+    {
+        printf("Error: <nthreads> must be 1 (serial version)\n");
+        exit(1);
+    }
+
+    // alloc spaces for the option data
+    data = new OptionData[numOptions];
+    prices = new AD_real[numOptions];
+    for (loopnum = 0; loopnum < numOptions; ++loopnum)
+    {
+        rv = fscanf(file, INPUT_LINE_FORMAT,
+                    &data[loopnum].s,
+                    &data[loopnum].strike,
+                    &data[loopnum].r,
+                    &data[loopnum].divq,
+                    &data[loopnum].v,
+                    &data[loopnum].t,
+                    &data[loopnum].OptionType,
+                    &data[loopnum].divs,
+                    &data[loopnum].DGrefval);
+
+        if (rv != 9)
+        {
+            printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+            fclose(file);
+            exit(1);
+        }
+    }
+    rv = fclose(file);
+    if (rv != 0)
+    {
+        printf("ERROR: Unable to close file `%s'.\n", inputFile);
+        exit(1);
+    }
+
+    printf("Num of Options: %d\n", numOptions);
+    printf("Num of Runs: %d\n", NUM_RUNS);
+
+    const int LINESIZE = 64;
+
+    buffer = new AD_real[5 * numOptions + LINESIZE];
+    sptprice = (AD_real *)(((unsigned long long)buffer + PAD) & ~(LINESIZE - 1));
+    strike = sptprice + numOptions;
+    rate = strike + numOptions;
+    volatility = rate + numOptions;
+    otime = volatility + numOptions;
+
+    buffer2 = new int[numOptions + LINESIZE];
+    otype = (int *)(((unsigned long long)buffer2 + PAD) & ~(LINESIZE - 1));
+
+    for (i = 0; i < numOptions; i++)
+    {
+        otype[i] = (data[i].OptionType == 'P') ? 1 : 0;
+        sptprice[i] = data[i].s;
+        strike[i] = data[i].strike;
+        rate[i] = data[i].r;
+        volatility[i] = data[i].v;
+        otime[i] = data[i].t;
+    }
+
+    printf("Size of data: %ld\n", numOptions * (sizeof(OptionData) + sizeof(int)));
+
+    // serial version
+    // ---------------------------- bs_thread() ------------------------------
+    int k, j;
+    AD_real price;
+    fptype priceDelta;
+    int start = 0 * (numOptions / nThreads);
+    int end = start + (numOptions / nThreads);
+
+    AD_begin();
+    AD_enable_source_aggregation();
+
+    for (j = 0; j < NUM_RUNS; j++)
+    {
+        for (k = start; k < end; k++)
+        {
+            /* Calling main function to calculate option value based on
+             * Black & Scholes's equation.
+             */
+            price = adapt::BlkSchlsEqEuroNoDiv(sptprice[k], strike[k],
+                                               rate[k], volatility[k], otime[k],
+                                               otype[k]);
+            prices[k] = price;
+
+#ifdef ERR_CHK
+            priceDelta = data[k].DGrefval - price;
+            if (fabs(priceDelta) >= 1e-4)
+            {
+                printf("Error on %d. Computed=%.5f, Ref=%.5f, Delta=%.5f\n",
+                       k, price, data[k].DGrefval, priceDelta);
+                numError++;
+            }
+#endif
+        }
+    }
+
+    AD_report();
+    AD_end();
+    // -------------------------- bs_thread END -----------------------------
+
+    // Write prices to output file
+    file = fopen(outputFile, "w");
+    if (file == NULL)
+    {
+        printf("ERROR: Unable to open file `%s'.\n", outputFile);
+        exit(1);
+    }
+    rv = fprintf(file, "%i\n", numOptions);
+    if (rv < 0)
+    {
+        printf("ERROR: Unable to write to file `%s'.\n", outputFile);
+        fclose(file);
+        exit(1);
+    }
+    for (i = 0; i < numOptions; i++)
+    {
+        rv = fprintf(file, "%.18f\n", AD_value(prices[i]));
+        if (rv < 0)
+        {
+            printf("ERROR: Unable to write to file `%s'.\n", outputFile);
+            fclose(file);
+            exit(1);
+        }
+    }
+    rv = fclose(file);
+    if (rv != 0)
+    {
+        printf("ERROR: Unable to close file `%s'.\n", outputFile);
+        exit(1);
+    }
+
+    delete[] buffer2;
+    delete[] buffer;
+    delete[] data;
+    delete[] prices;
+
+    return 0;
+}
---
+++ blackscholes/blackscholes-adapt.hpp
@@ -0,0 +1,251 @@
+#include <cmath>
+
+#include "adapt.h"
+#include "adapt-impl.cpp"
+
+namespace adapt
+{
+
+    AD_real BlkSchlsEqEuroNoDiv(AD_real sptprice,
+                                AD_real strike, AD_real rate, AD_real volatility,
+                                AD_real time, int otype)
+    {
+        AD_INDEPENDENT(sptprice, "sptprice");
+        AD_INDEPENDENT(strike, "strike");
+        AD_INDEPENDENT(rate, "rate");
+        AD_INDEPENDENT(volatility, "volatility");
+        AD_INDEPENDENT(time, "time");
+
+        AD_real OptionPrice;
+
+        // local private working variables for the calculation
+        AD_real xStockPrice;
+        AD_real xStrikePrice;
+        AD_real xRiskFreeRate;
+        AD_real xVolatility;
+        AD_real xTime;
+        AD_real xSqrtTime;
+
+        AD_real logValues;
+        AD_real xLogTerm;
+        AD_real xD1;
+        AD_real xD2;
+        AD_real xPowerTerm;
+        AD_real xDen;
+        AD_real d1;
+        AD_real d2;
+        AD_real FutureValueX;
+        AD_real NofXd1;
+        AD_real NofXd2;
+        AD_real NegNofXd1;
+        AD_real NegNofXd2;
+
+        AD_real InputX;
+
+        int sign;
+
+        AD_real OutputX;
+        AD_real xInput;
+        AD_real xNPrimeofX;
+        AD_real expValues;
+        AD_real xK2;
+        AD_real xK2_2, xK2_3;
+        AD_real xK2_4, xK2_5;
+        AD_real xLocal, xLocal_1;
+        AD_real xLocal_2, xLocal_3;
+        AD_real inv_sqrt_2xPI = 0.39894228040143270286;
+
+        xStockPrice = sptprice;
+        xStrikePrice = strike;
+        xRiskFreeRate = rate;
+        xVolatility = volatility;
+
+        AD_INTERMEDIATE(xStockPrice, "xStockPrice");
+        AD_INTERMEDIATE(xStrikePrice, "xStrikePrice");
+        AD_INTERMEDIATE(xRiskFreeRate, "xRiskFreeRate");
+        AD_INTERMEDIATE(xVolatility, "xVolatility");
+
+        xTime = time;
+        AD_INTERMEDIATE(xTime, "xTime");
+
+        xSqrtTime = sqrt(xTime);
+        AD_INTERMEDIATE(xSqrtTime, "xSqrtTime");
+
+        logValues = log(sptprice / strike);
+        AD_INTERMEDIATE(logValues, "logValues");
+
+        xLogTerm = logValues;
+        AD_INTERMEDIATE(xLogTerm, "xLogTerm");
+
+        xPowerTerm = xVolatility * xVolatility;
+        xPowerTerm = xPowerTerm * 0.5;
+        AD_INTERMEDIATE(xPowerTerm, "xPowerTerm");
+
+        xD1 = xRiskFreeRate + xPowerTerm;
+        xD1 = xD1 * xTime;
+        xD1 = xD1 + xLogTerm;
+
+        xDen = xVolatility * xSqrtTime;
+        AD_INTERMEDIATE(xDen, "xDen");
+        xD1 = xD1 / xDen;
+        AD_INTERMEDIATE(xD1, "xD1");
+        xD2 = xD1 - xDen;
+        AD_INTERMEDIATE(xD2, "xD2");
+
+        // ---------------------------- NofXd1 = CNDF(d1) --------------------------
+        InputX = xD1;
+
+        // Check for negative value of InputX
+        if (InputX < 0.0)
+        {
+            InputX = -InputX;
+            sign = 1;
+        }
+        else
+            sign = 0;
+
+        AD_INTERMEDIATE(InputX, "InputX");
+
+        xInput = InputX;
+        AD_INTERMEDIATE(xInput, "xInput");
+
+        // Compute NPrimeX term common to both four & six decimal accuracy calcs
+        expValues = exp(-0.5f * InputX * InputX);
+        AD_INTERMEDIATE(expValues, "expValues");
+        xNPrimeofX = expValues;
+        xNPrimeofX = xNPrimeofX * inv_sqrt_2xPI;
+        AD_INTERMEDIATE(xNPrimeofX, "xNPrimeofX");
+
+        xK2 = 0.2316419 * xInput;
+        xK2 = 1.0 + xK2;
+        xK2 = 1.0 / xK2;
+        AD_INTERMEDIATE(xK2, "xK2");
+        xK2_2 = xK2 * xK2;
+        AD_INTERMEDIATE(xK2_2, "xK2_2");
+        xK2_3 = xK2_2 * xK2;
+        AD_INTERMEDIATE(xK2_3, "xK2_3");
+        xK2_4 = xK2_3 * xK2;
+        AD_INTERMEDIATE(xK2_4, "xK2_4");
+        xK2_5 = xK2_4 * xK2;
+        AD_INTERMEDIATE(xK2_5, "xK2_5");
+
+        xLocal_1 = xK2 * 0.319381530;
+        xLocal_2 = xK2_2 * (-0.356563782);
+        xLocal_3 = xK2_3 * 1.781477937;
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        xLocal_3 = xK2_4 * (-1.821255978);
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        xLocal_3 = xK2_5 * 1.330274429;
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        AD_INTERMEDIATE(xLocal_2, "xLocal_2");
+
+        xLocal_1 = xLocal_2 + xLocal_1;
+        AD_INTERMEDIATE(xLocal_1, "xLocal_1");
+        xLocal = xLocal_1 * xNPrimeofX;
+        xLocal = 1.0 - xLocal;
+        AD_INTERMEDIATE(xLocal, "xLocal");
+
+        OutputX = xLocal;
+
+        if (sign)
+        {
+            OutputX = 1.0 - OutputX;
+        }
+        AD_INTERMEDIATE(OutputX, "OutputX");
+
+        NofXd1 = OutputX;
+        AD_INTERMEDIATE(NofXd1, "NofXd1");
+        // -------------------------- NofXd1 = CNDF(d1) END ------------------------
+
+        // ---------------------------- NofXd2 = CNDF(d2) --------------------------
+        InputX = xD2;
+
+        // Check for negative value of InputX
+        if (InputX < 0.0)
+        {
+            InputX = -InputX;
+            sign = 1;
+        }
+        else
+            sign = 0;
+
+        AD_INTERMEDIATE(InputX, "InputX");
+
+        xInput = InputX;
+        AD_INTERMEDIATE(xInput, "xInput");
+
+        // Compute NPrimeX term common to both four & six decimal accuracy calcs
+        expValues = exp(-0.5f * InputX * InputX);
+        AD_INTERMEDIATE(expValues, "expValues");
+        xNPrimeofX = expValues;
+        xNPrimeofX = xNPrimeofX * inv_sqrt_2xPI;
+        AD_INTERMEDIATE(xNPrimeofX, "xNPrimeofX");
+
+        xK2 = 0.2316419 * xInput;
+        xK2 = 1.0 + xK2;
+        xK2 = 1.0 / xK2;
+        AD_INTERMEDIATE(xK2, "xK2");
+        xK2_2 = xK2 * xK2;
+        AD_INTERMEDIATE(xK2_2, "xK2_2");
+        xK2_3 = xK2_2 * xK2;
+        AD_INTERMEDIATE(xK2_3, "xK2_3");
+        xK2_4 = xK2_3 * xK2;
+        AD_INTERMEDIATE(xK2_4, "xK2_4");
+        xK2_5 = xK2_4 * xK2;
+        AD_INTERMEDIATE(xK2_5, "xK2_5");
+
+        xLocal_1 = xK2 * 0.319381530;
+        xLocal_2 = xK2_2 * (-0.356563782);
+        xLocal_3 = xK2_3 * 1.781477937;
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        xLocal_3 = xK2_4 * (-1.821255978);
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        xLocal_3 = xK2_5 * 1.330274429;
+        AD_INTERMEDIATE(xLocal_3, "xLocal_3");
+        xLocal_2 = xLocal_2 + xLocal_3;
+        AD_INTERMEDIATE(xLocal_2, "xLocal_2");
+
+        xLocal_1 = xLocal_2 + xLocal_1;
+        AD_INTERMEDIATE(xLocal_1, "xLocal_1");
+        xLocal = xLocal_1 * xNPrimeofX;
+        xLocal = 1.0 - xLocal;
+        AD_INTERMEDIATE(xLocal, "xLocal");
+
+        OutputX = xLocal;
+
+        if (sign)
+        {
+            OutputX = 1.0 - OutputX;
+        }
+        AD_INTERMEDIATE(OutputX, "OutputX");
+
+        NofXd2 = OutputX;
+        AD_INTERMEDIATE(NofXd2, "NofXd2");
+        // -------------------------- NofXd2 = CNDF(d2) END ------------------------
+
+        FutureValueX = strike * (exp(-(rate) * (time)));
+        AD_INTERMEDIATE(FutureValueX, "FutureValueX");
+        if (otype == 0)
+        {
+            OptionPrice = (sptprice * NofXd1) - (FutureValueX * NofXd2);
+        }
+        else
+        {
+            NegNofXd1 = (1.0 - NofXd1);
+            AD_INTERMEDIATE(NegNofXd1, "NegNofXd1");
+            NegNofXd2 = (1.0 - NofXd2);
+            AD_INTERMEDIATE(NegNofXd2, "NegNofXd2");
+            OptionPrice = (FutureValueX * NegNofXd2) - (sptprice * NegNofXd1);
+        }
+
+        AD_DEPENDENT(OptionPrice, "OptionPrice", 0.0);
+
+        return OptionPrice;
+    }
+
+}
