0a427a3e8056853e3bb2f63e8d8f1516b1382d2d
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-08-11 05:23:06 +0200

    Add profiling code to arclength

--- arclength/clad-main.cpp
+++ arclength/clad-main.cpp
@@ -5,15 +5,22 @@
 
 #include "Derivative.hpp"
 
+// #include <gperftools/profiler.h>
+
 int main()
 {
     //   auto df = clad::estimate_error(do_fun);
     double ds1 = 0, dt1 = 0;
     double fin_error = 0;
 
-    clad::resetErrors();
-
+    // ProfilerStart("arclength.prof");
     double result = do_fun(0, 0);
+    // ProfilerStop();
+
+
+    // ProfilerStart("clad.prof");
+    clad::resetErrors();
     clad::do_fun_grad(0, 0, &ds1, &dt1, fin_error);
     clad::printErrorReport();
+    // ProfilerStop();
 }
