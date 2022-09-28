ce404d4a05cfa1d313ad1f9f357259b17ce25414
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-28 15:15:13 +0200

    Add memory benchmark using time command

--- runner.sh
+++ runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT_PROCESSING_SCRIPT=$ws_dir/clad-fp-error-est-examples/process_output.py
+
 bench_gen() {
     benchall -O3 ${1}.cpp ${2} -o ${1}.out -L$ws_dir/benchmark/build/src
 }
@@ -13,18 +15,30 @@ gen_mix() {
 }
 
 bench_run() {
-    echo "==================== Running: $2 ===================="
-    $1 --benchmark_filter=${2}\$ --benchmark_format=json >$3
+    # Takes 3 arguments in order: benchmark executable name; benchmark_filter input; benchmark_out file path
+    /usr/bin/time -v $1 --benchmark_filter=${2}\$ --benchmark_out_format=json --benchmark_out=$3 2>&1 | grep "Maximum resident set size" | sed "s/[^0-9]//g"
+}
+
+run_bench() {
+    # Takes 3 arguments in order: benchmark executable name; benchmark_filter input/benchmark name; json file path/benchmark_out file path
+    BENCHMARK_EXEC=$1
+    BENCHMARK_NAME=$2
+    JSON_FILE=$3
+    FILE=$ws_dir/results.txt
+
+    echo "==================== Running: $BENCHMARK_NAME ADAPT Vs. CLAD ===================="
+    MEM_USAGE=$(bench_run $BENCHMARK_EXEC $BENCHMARK_NAME $JSON_FILE)
+    python3 $OUTPUT_PROCESSING_SCRIPT $JSON_FILE $MEM_USAGE >>$FILE
 }
 
 run_adc() {
-    bench_run ./benchmark.out $1 /tmp/b.json
-    python3 $ws_dir/clad-fp-error-est-examples/process_json.py /tmp/b.json >>$ws_dir/results.txt
+    # Takes 1 argument: benchmark_filter input/benchmark name;
+    run_bench ./benchmark.out $1 /tmp/b.json
 }
 
 run_mix() {
-    bench_run ./benchmark-mixed.out $1 /tmp/m.json
-    python3 $ws_dir/clad-fp-error-est-examples/process_json.py /tmp/m.json >>$ws_dir/results.txt
+    # Takes 1 argument: benchmark_filter input/benchmark name;
+    run_bench ./benchmark-mixed.out $1 /tmp/m.json
 }
 
 echo "" >$ws_dir/results.txt
@@ -47,44 +61,50 @@ echo "" >$ws_dir/results.txt
 # run_adc ErrorEstimateArcLenAdapt/10000000
 # cd ..
 
-# cd blackscholes
+# echo "" >$ws_dir/results.txt
+
+cd blackscholes
+gen_adc
+run_adc BlackScholes/0
+run_adc BlackScholes/1
+run_adc BlackScholes/2
+run_adc BlackScholes/3
+run_adc BlackScholes/4
+run_adc BlackScholes_Clad/0
+run_adc BlackScholes_Clad/1
+run_adc BlackScholes_Clad/2
+run_adc BlackScholes_Clad/3
+run_adc BlackScholes_Clad/4
+run_adc BlackScholes_Adapt/0
+run_adc BlackScholes_Adapt/1
+run_adc BlackScholes_Adapt/2
+cd ..
+
+# echo "" >$ws_dir/results.txt
+
+# cd HPCCG
 # gen_adc
-# run_adc ErrorEstimateBlkSol/0
-# run_adc ErrorEstimateBlkSol/1
-# run_adc ErrorEstimateBlkSol/2
-# run_adc ErrorEstimateBlkSol/3
-# run_adc ErrorEstimateBlkSol/4
-# run_adc ErrorEstimateBlkSolClad/0
-# run_adc ErrorEstimateBlkSolClad/1
-# run_adc ErrorEstimateBlkSolClad/2
-# run_adc ErrorEstimateBlkSolClad/3
-# run_adc ErrorEstimateBlkSolClad/4
-# run_adc ErrorEstimateBlkSolAdapt/0
-# run_adc ErrorEstimateBlkSolAdapt/1
-# run_adc ErrorEstimateBlkSolAdapt/2
+# run_adc ErrorEstimateHPCCG/20/30/10
+# run_adc ErrorEstimateHPCCG/20/30/20
+# run_adc ErrorEstimateHPCCG/20/30/40
+# run_adc ErrorEstimateHPCCG/20/30/80
+# run_adc ErrorEstimateHPCCG/20/30/160
+# run_adc ErrorEstimateHPCCG/20/30/320
+# run_adc ErrorEstimateHPCCGClad/20/30/10
+# run_adc ErrorEstimateHPCCGClad/20/30/20
+# run_adc ErrorEstimateHPCCGClad/20/30/40
+# run_adc ErrorEstimateHPCCGClad/20/30/80
+# run_adc ErrorEstimateHPCCGClad/20/30/160
+# run_adc ErrorEstimateHPCCGClad/20/30/320
+# run_adc ErrorEstimateHPCCGAdapt/20/30/10
+# run_adc ErrorEstimateHPCCGAdapt/20/30/20
+# run_adc ErrorEstimateHPCCGAdapt/20/30/40
+# run_adc ErrorEstimateHPCCGAdapt/20/30/80
+# run_adc ErrorEstimateHPCCGAdapt/20/30/160
+# run_adc ErrorEstimateHPCCGAdapt/20/30/320
 # cd ..
 
-cd HPCCG
-gen_adc
-run_adc ErrorEstimateHPCCG/20/30/10
-run_adc ErrorEstimateHPCCG/20/30/20
-run_adc ErrorEstimateHPCCG/20/30/40
-run_adc ErrorEstimateHPCCG/20/30/80
-run_adc ErrorEstimateHPCCG/20/30/160
-run_adc ErrorEstimateHPCCG/20/30/320
-run_adc ErrorEstimateHPCCGClad/20/30/10
-run_adc ErrorEstimateHPCCGClad/20/30/20
-run_adc ErrorEstimateHPCCGClad/20/30/40
-run_adc ErrorEstimateHPCCGClad/20/30/80
-run_adc ErrorEstimateHPCCGClad/20/30/160
-run_adc ErrorEstimateHPCCGClad/20/30/320
-run_adc ErrorEstimateHPCCGAdapt/20/30/10
-run_adc ErrorEstimateHPCCGAdapt/20/30/20
-run_adc ErrorEstimateHPCCGAdapt/20/30/40
-run_adc ErrorEstimateHPCCGAdapt/20/30/80
-run_adc ErrorEstimateHPCCGAdapt/20/30/160
-run_adc ErrorEstimateHPCCGAdapt/20/30/320
-cd ..
+# echo "" >$ws_dir/results.txt
 
 # cd kmeans
 # gen_adc
@@ -104,6 +124,8 @@ cd ..
 # run_adc ErrorEstimateKMeansAdapt/3
 # cd ..
 
+# echo "" >$ws_dir/results.txt
+
 # cd simpsons
 # gen_adc
 # run_adc ErrorEstimateSimp/10000
@@ -121,6 +143,8 @@ cd ..
 # run_adc ErrorEstimateSimpAdapt/1000000
 # run_adc ErrorEstimateSimpAdapt/10000000
 # cd ..
+
+# echo "" >$ws_dir/results.txt
 
 # cd arclength
 # gen_mix
@@ -140,6 +164,8 @@ cd ..
 # run_mix ArcLenHighPrec/10000000
 # run_mix ArcLenHighPrec/100000000
 # cd ..
+
+# echo "" >$ws_dir/results.txt
 
 # cd simpsons
 # gen_mix
