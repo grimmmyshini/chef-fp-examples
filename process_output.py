f5783c6b60d4161877f536c5517fc7d5bb2b9cf7
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-28 15:15:50 +0200

    Add printing of memory usage

--- process_output.py
+++ process_output.py
@@ -4,6 +4,7 @@ import json
 import sys
 
 f = open(sys.argv[1])
+memory_usage = sys.argv[2]
 
 data = json.load(f)
 
@@ -12,4 +13,4 @@ name, inp = data['benchmarks'][i]['name'].split("/", 1)
 time = data['benchmarks'][i]['real_time']
 mbu = data['benchmarks'][i].get('max_bytes_used', -1)
 
-print('{:>25}, {:>10}, {:15.2f}, {:20d}'.format(name, inp, time, mbu))
+print('{:>25}, {:>10}, {:15.2f}, {:20d}, {:>20}'.format(name, inp, time, mbu, memory_usage))
