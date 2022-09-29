964556c61fc1e6577ad40a5e39cd089474005c9d
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-29 11:51:05 +0200

    Make process_output output the heading

--- process_output.py
+++ process_output.py
@@ -3,14 +3,25 @@
 import json
 import sys
 
-f = open(sys.argv[1])
-memory_usage = sys.argv[2]
+if len(sys.argv) == 3:
+    f = open(sys.argv[1])
+    memory_usage = sys.argv[2]
 
-data = json.load(f)
+    data = json.load(f)
 
-i = 0
-name, inp = data['benchmarks'][i]['name'].split("/", 1)
-time = data['benchmarks'][i]['real_time']
-mbu = data['benchmarks'][i].get('max_bytes_used', -1)
+    i = 0
+    name, inp = data['benchmarks'][i]['name'].split("/", 1)
+    time = data['benchmarks'][i]['real_time']
+    time_unit = data['benchmarks'][i]['time_unit']
+    mbu = data['benchmarks'][i].get('max_bytes_used', -1)
+    print('{:>50}, {:>10}, {:25f}, {:>4}, {:20d}, {:>20}'.format(name, inp, time, time_unit, mbu, memory_usage))
+elif len(sys.argv) == 1:
+    name = "Name"
+    inp = "Data/Iters"
+    time = "Time"
+    time_unit = "unit"
+    mbu = "Memory (gbench)"
+    memory_usage = "Memory(time)"
+    print('{:>50}, {:>10}, {:>25}, {:>4}, {:>20}, {:>20}'.format(name, inp, time, time_unit, mbu, memory_usage))
 
-print('{:>25}, {:>10}, {:15.2f}, {:20d}, {:>20}'.format(name, inp, time, mbu, memory_usage))
+
