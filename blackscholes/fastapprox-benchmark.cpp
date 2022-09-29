4f9fda4ac29cecd370fc17862b7c371c95456a1a
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-29 18:13:08 +0200

    Add benchmark for fastapprox

---
+++ blackscholes/fastapprox-benchmark.cpp
@@ -0,0 +1,47 @@
+#include "benchmark/benchmark.h"
+#include "../benchmark-utils/memory-manager.hpp"
+
+#include <random>
+#include <cmath>
+#include "fastapprox/fastapprox/src/fastonebigheader.h"
+
+void exp_bench(benchmark::State &state) {
+    double lower_bound = 0;
+    double upper_bound = 10000;
+    std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+    std::default_random_engine re;
+
+    int iterations = state.range(0);
+
+    for (auto _ : state)
+    {
+        for (int i = 0; i < iterations; i++) {
+            double x = unif(re);
+            double e = exp(x);
+            benchmark::DoNotOptimize(e);
+        }
+    }
+}
+
+void fastexp_bench(benchmark::State &state) {
+    double lower_bound = 0;
+    double upper_bound = 10000;
+    std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+    std::default_random_engine re;
+
+    int iterations = state.range(0);
+
+    for (auto _ : state)
+    {
+        for (int i = 0; i < iterations; i++) {
+            double x = unif(re);
+            double e = fastexp(x);
+            benchmark::DoNotOptimize(e);
+        }
+    }
+}
+
+BENCHMARK(exp_bench)->RangeMultiplier(10)->Range(1, 100000000);
+BENCHMARK(fastexp_bench)->RangeMultiplier(10)->Range(1, 100000000);
+
+BENCHMARK_MAIN();
